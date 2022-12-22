d7c745ef16eaf2534b309f46e9aec6a2872524c3
Cavalcante <103554466+flprc@users.noreply.github.com>
2022-12-22 14:48:20 -0300

    Initial Commit

---
+++ js.js
@@ -0,0 +1,60 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordtwo = document.getElementById('passwordtwo');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const passwordtwoValue = passwordtwo.value.trim();
+
+    if(usernameValue === '') {
+        errorValidation(username, 'Preencha este campo');
+    } else {
+        successValidation(username);
+    };
+
+    if(emailValue === '') {
+        errorValidation(email, 'Preencha este campo');
+    } else {
+        successValidation(email);
+    };
+
+    if(passwordValue === '') {
+        errorValidation(password, 'Preencha este campo');
+    } else if(passwordValue.length < 8) {
+        errorValidation(password, 'Senha deve ter mais que 8 caracteres');
+    } else {
+        successValidation(password);
+    };
+
+    if(passwordtwoValue === '') {
+        errorValidation(passwordtwo, 'Preencha este campo')
+    } else if (passwordValue !== passwordtwoValue) {
+        errorValidation(passwordtwo, 'Senhas não são as mesmas');
+    } else {
+        successValidation(passwordtwo);
+    };
+};
+
+function errorValidation(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error';
+}
+
+function successValidation(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success';
+}
